f119d4d3af4964b0c46a6d7c254028e883556d17
TheR16H <rashawn5hall@gmail.com>
2024-08-22 23:03:42 -0400

    deleted commented out code

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,8 +12,7 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => { // Create the connection to the database 
+export const putDb = async (content) => { 
 	const jateDb = await openDB('jate', 1); 
 
 	const tx = jateDb.transaction('jate', 'readwrite');
@@ -22,7 +21,6 @@ export const putDb = async (content) => { // Create the connection to the databa
 	const result = await request;
 	console.error('putDb not implemented');
 }
-// TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
 
 initdb();
